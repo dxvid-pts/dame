61242a87e9251ca33e05a1cd07588ea70097cc54
Philipp E <philipp3923@icloud.com>
2022-06-14 22:09:37 +0200

    socket join angefangen

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+import {joinRoom} from "utils/rooms";
+
 const Constants = require("shared/constants")
 
 const http = require('http').createServer();
@@ -12,6 +14,19 @@ io.on('connection', (socket) => {
     socket.on('message', (message) => {
         console.log(message);
         io.emit('message', `${socket.id.substr(0, 2)} said ${message}`);
+    });
+
+    socket.on('join', (room) => {
+        if(room == null){
+            //send back a created room with id!
+        }
+        
+        console.log(socket.id);
+        tryJoin = joinRoom(room, socket.id);
+
+        //send back result of joinRoom and cick user if it fails
+
+
     });
 
 });
