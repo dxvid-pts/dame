7786222b6c3d5b0e5f2127a7f1156a32c707a804
MetaEMK <jan-benecke@t-online.de>
2022-06-16 00:24:43 +0200

    Create basic ChatMessage

---
+++ client/src/components/ChatMessage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./ChatMessage.css"
+
+const ChatMessage = ({ message }) => {
+    return (
+        <div 
+            className={`message-item ${
+            message.ownedByCurrentUser ? "my-message" : "received-message"}
+            `}
+        >
+            <div className="message-body-container">
+                {!message.ownedByCurrentUser && (
+                <div className="message-user-name">{message.user.name}</div>
+                )}
+                <div className="message-body">{message.body}</div>
+            </div>
+        </div>
+    )
+}
+export default ChatMessage;
