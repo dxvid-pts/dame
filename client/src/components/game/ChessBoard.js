389b95e06d5f4caee4032a1fb9306f5f21bf9144
david.peters <david.peters@mway.io>
2022-07-05 16:35:19 +0200

    add backgroundImages

--- client/src/components/game/ChessBoard.js
+++ client/src/components/game/ChessBoard.js
@@ -48,22 +48,26 @@ export function ChessBoardTile(props) {
     if (props.column % 2 === 0) {
         white = !white;
     }
-    let tileColor = white ? Constants.COLOR_CHESSBOARD_EVEN : Constants.COLOR_CHESSBOARD_ODD;
+
+    const opacity = 0.4;
+    let tileColor = ""; //white ? Constants.COLOR_CHESSBOARD_EVEN : Constants.COLOR_CHESSBOARD_ODD;
 
     if (isHovered) {
-        tileColor = mixColors(tileColor, '#B5FDA4');
+        tileColor = "rgba(181,253,164,"+opacity+")";
     }
 
     //render highlighted fields
     for (let position of props.globalState.highlightedFields) {
         if (position.row === props.row && position.column === props.column) {
-            tileColor = mixColors(tileColor, '#FF0000');
+            tileColor =  tileColor = "rgba(255,0,0,"+opacity+")";
+            //tileColor = mixColors(tileColor, '#FF0000');
         }
     }
 
     //check if selected
     if (props.globalState.selectedTile != null && props.globalState.selectedTile.row === props.row && props.globalState.selectedTile.column === props.column) {
-        tileColor = mixColors(tileColor, '#f5ad42');
+        tileColor =  tileColor = "rgba(245,173,66,"+opacity+")";
+       // tileColor = mixColors(tileColor, '#f5ad42');
     }
 
     let p = null;
@@ -76,6 +80,8 @@ export function ChessBoardTile(props) {
             break;
     }
 
+    const backgroundImgUrl = white ? Constants.BOARD_WHITE : Constants.BOARD_BLACK;
+
     return <div
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
@@ -83,9 +89,14 @@ export function ChessBoardTile(props) {
         style={{
             width: tileSize,
             height: tileSize,
-            backgroundColor: tileColor,
+            backgroundImage: "url(" + backgroundImgUrl + ")"
         }}>
-        {p}
+        <div style={{
+            width: tileSize,
+            height: tileSize,
+            backgroundColor: tileColor,
+        }}
+        >{p}</div>
     </div>;
 }
 
--- shared/constants.js
+++ shared/constants.js
@@ -4,7 +4,9 @@ const constants = {
     COLOR_CHESSBOARD_EVEN: "#FFFF00",
     COLOR_CHESSBOARD_ODD: "#0000FF",
     PLAYER_SVG_WHITE: "https://upload.wikimedia.org/wikipedia/commons/a/a0/Circle_-_black_simple.svg",
-    PLAYER_SVG_BLACK: "https://ouch-cdn2.icons8.com/irEklyXBroENkWIOaPY8ympA2-aBGNXzhP8EIRIojAo/rs:fit:256:256/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvNDAz/Lzc1YWZjOWM0LTA4/YTItNGIzZS1hYmU3/LWVhYmU0OTU4Mzkx/Ni5zdmc.png"
+    PLAYER_SVG_BLACK: "https://ouch-cdn2.icons8.com/irEklyXBroENkWIOaPY8ympA2-aBGNXzhP8EIRIojAo/rs:fit:256:256/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvNDAz/Lzc1YWZjOWM0LTA4/YTItNGIzZS1hYmU3/LWVhYmU0OTU4Mzkx/Ni5zdmc.png",
+    BOARD_WHITE: "https://img.freepik.com/free-photo/light-wood-texture-with-natural-pattern-wooden-planks-background-pastel-colors_84485-1308.jpg?w=2000",
+    BOARD_BLACK: "https://img.rawpixel.com/private/static/images/website/2022-05/rm187-nunoon-66.jpg?w=1200&h=1200&dpr=1&fit=clip&crop=default&fm=jpg&q=75&vib=3&con=3&usm=15&cs=srgb&bg=F4F4F3&ixlib=js-2.2.1&s=63879f07dbf0c2b4cfdefe3ea168be33"
 }
 
 module.exports = constants
