99db7a8fdf4f670d42f9ec35c84dd6cbb604dba5
Philipp E <philipp3923@icloud.com>
2022-07-27 00:02:04 +0200

    fix

--- client/src/pages/game/game.js
+++ client/src/pages/game/game.js
@@ -1,12 +1,14 @@
 import GameArea from "../../components/game/GameArea";
 import ChatArea from "../../components/chat/ChatArea";
+import Header from "../../components/header";
 
 export default function Index(props) {
     return (
         <div className="grid-container">
-            <div id="header">Header</div>
-            <GameArea socket={props.socket}></GameArea>
-            <ChatArea socket={props.socket} />
-        </div>
+        <Header></Header>
+        <div style={{gridArea: "blackbox2", backgroundColor: "black"}}></div>
+        <GameArea socket={socket}></GameArea>
+        <ChatArea socket={socket}/>
+        <div style={{gridArea: "blackbox", backgroundColor: "black"}}></div></div>
     );
 }
