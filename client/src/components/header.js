6d95dd4498b7f14b0abd414fcd17f648869ee57b
david.peters <david.peters@mway.io>
2022-07-26 16:02:16 +0200

    delete props

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,4 +1,4 @@
-export default function Header(props) {
+export default function Header() {
     return <div id="header" style={{backgroundColor: "black", borderBottom: "3px solid white"}}><img
         //https://www10.flamingtext.com/net-fu/jobs/27830759481728562.html
         src="https://i.ibb.co/DVzq5Y5/coollogo-com-11808154-1.gif" alt="logo" height={30}></img>
