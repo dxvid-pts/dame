a49f89b01fbdeabf2d2408299817b7a88bfc3fcc
david.peters <david.peters@mway.io>
2022-07-26 16:01:29 +0200

    use permalink

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,5 +1,6 @@
 export default function Header(props) {
-    return <div id="header" style={{backgroundColor: "black",borderBottom:"3px solid white"}}><img
-        src="https://de24-engine.flamingtext.com/netfu/tmp28007/coollogo_com-11808154.gif" alt="logo" height={30}></img>
+    return <div id="header" style={{backgroundColor: "black", borderBottom: "3px solid white"}}><img
+        //https://www10.flamingtext.com/net-fu/jobs/27830759481728562.html
+        src="https://i.ibb.co/DVzq5Y5/coollogo-com-11808154-1.gif" alt="logo" height={30}></img>
     </div>;
 }
