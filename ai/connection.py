921de10e86d6029588d04ace0d8d42d9650d32df
Luis <64305142+xTerradon@users.noreply.github.com>
2022-08-01 06:33:53 +0200

    Create connection.py

---
+++ ai/connection.py
@@ -0,0 +1,34 @@
+import asyncio
+
+import websockets
+import json
+
+import ai_loader
+
+class Connection:
+
+    def __init__(self):
+        asyncio.run(self.main())
+
+    async def handler(self, websocket):
+        while True:
+            message = await websocket.recv()
+            print(message)
+
+            # get request_id, strength, board, player_turn, must_move_xy
+            move = ai_loader.get_move(board, player_turn, must_move_xy, strength)
+
+            event = {
+                "id": request_id,
+                "strength": strength,
+                "from_x": move[0],
+                "from_y": move[1],
+                "to_x": move[2],
+                "to_x": move[3],
+            }
+            await websocket.send(json.dumps(event))
+
+
+    async def main(self):
+        async with websockets.serve(self.handler, "", 8001):
+            await asyncio.Future()  # run forever
